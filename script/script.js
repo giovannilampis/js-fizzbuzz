13f79cb581dabe64483bbc1331beeae517ae4e30
giovannilampis <giolampis@hotmail.com>
2023-03-24 15:42:01 +0100

    add comments to script

--- script/script.js
+++ script/script.js
@@ -2,12 +2,19 @@
 
 for (let i = 1; i <= 100; i++) {
     
+    // STAMPA "FIZZBUZZ" anzi che i numeri che sono multipli sia di 3 che di 5
     if ( i % 15 == 0 ) {
         console.log(FizzBuzz);
+
+    // STAMPA "FIZZ" anzi che i numeri che sono multipli di 3     
     } else if ( (i != 3) && (i % 3) == 0) {
         console.log(Fizz);
+
+     // STAMPA "BUZZ" anzi che i numeri che sono multipli di 5     
     } else if ( (i != 5) && (i % 5) == 0) {
         console.log(Buzz);
+
+    // IN TUTTI GLI ALTRI CASI STAMPA IL NUMERO APPROPRIATO    
     } else {
         console.log(i);
     }
