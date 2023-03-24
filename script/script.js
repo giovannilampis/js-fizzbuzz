0f1f7c93f0e4c6f9e0a375df219902c761c1b995
giovannilampis <giolampis@hotmail.com>
2023-03-24 15:39:54 +0100

    add for cycle to script.js

--- script/script.js
+++ script/script.js
@@ -1,6 +1,15 @@
 "use strict"
 
-for (let i = 0; i <= 100; i++) {
-    console.log(i);
+for (let i = 1; i <= 100; i++) {
+    
+    if ( i % 15 == 0 ) {
+        console.log(FizzBuzz);
+    } else if ( (i != 3) && (i % 3) == 0) {
+        console.log(Fizz);
+    } else if ( (i != 5) && (i % 5) == 0) {
+        console.log(Buzz);
+    } else {
+        console.log(i);
+    }
 }
 
